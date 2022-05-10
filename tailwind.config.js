8ce64f5d2231468b0fd0b8a8646bbb0008bf8226
Esto Triramdani N <estolagi@gmail.com>
2022-05-10 16:10:41 +0700

    tailwind plugin

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#BB1529',
+      },
+    },
+  },
+  plugins: [],
+};
