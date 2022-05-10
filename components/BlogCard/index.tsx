14d69efede41a6734d85a7605714271d6eb23ff5
Esto Triramdani N <estolagi@gmail.com>
2022-05-10 16:10:25 +0700

    blog card

---
+++ components/BlogCard/index.tsx
@@ -0,0 +1,19 @@
+export default function BlogCard() {
+  return (
+    <div className="overflow-hidden rounded-xl bg-slate-50">
+      <div className="p-3">
+        <p className="mb-3 text-sm uppercase text-slate-500">AUGust 13, 2021</p>
+        <h4 className="font-serif text-lg font-bold  text-slate-800">
+          10 Hilarious Cartoons That Depict Real-Life Problems of Programmers
+        </h4>
+      </div>
+      <div>
+        <img
+          className="h-52 w-full object-cover"
+          src="https://dummyimage.com/600x400/000/fff"
+          alt="image"
+        />
+      </div>
+    </div>
+  );
+}
