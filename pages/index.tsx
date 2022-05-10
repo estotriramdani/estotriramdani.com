023486d55d402b575c95f1eb3184afefd66963a9
Esto Triramdani N <estolagi@gmail.com>
2022-05-10 16:11:50 +0700

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,29 @@
-import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import BlogCard from '../components/BlogCard';
+import HeroSection from '../components/HeroSection';
+import Navigation from '../components/Navigation';
+import { Heading3 } from '../components/Typography';
 
-const Home = () => {
+export default function Home() {
   return (
     <>
       <Head>
-        <title>Esto Triramdani N - a software engineer</title>
-        <meta
-          name="description"
-          content="Esto Triramdani is a software engineer"
-        />
+        <title>Home</title>
       </Head>
+      <Navigation />
+      <main>
+        <HeroSection />
+        <div className="h-min-screen w-full bg-slate-100 px-4 py-3">
+          <Heading3 className="mb-3">Latest Posts</Heading3>
+          <ul>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+              <li key={item} className="mb-3">
+                <BlogCard key={item} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return {
-    redirect: {
-      destination: '/portfolios',
-      permanent: false,
-    },
-  };
-};
-
-export default Home;
+}
