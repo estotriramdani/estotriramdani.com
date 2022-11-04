5dd9a71151e7e8b14bef78d818b0349822fb93a1
Esto Triramdani N <estolagi@gmail.com>
2022-11-04 16:52:44 +0700

    to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <div className="mx-auto w-[400px] shadow-lg">
-    <Component {...pageProps} />
-    // </div>
+    <>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
 
